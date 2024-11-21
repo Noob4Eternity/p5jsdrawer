ab2fc345d3f1ec64ed49dcf46c985df8023800d6
noob4eternity <veddatar123@gmail.com>
2024-11-21 22:48:23 +0530

    api-key fix

--- gemini.js
+++ gemini.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 async function run(prompt) {
-  const genAI = new GoogleGenerativeAI("AIzaSyAvutJ4bNHuyVwd26MSqAuuyqVg84m8M7M");
+  const genAI = new GoogleGenerativeAI("API-KEY");
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
   const result = await model.generateContent([`${prompt}`]);
